fcf9bb80135b9ef0a1478b9f51e27719a59a0008
tomfigueroa Alberto <t.figueroa@javeriana.edu.co>
2023-10-13 15:50:30 +0000

    Consultas adelantadas

--- scripts/consuta_trabajo.sql
+++ scripts/consuta_trabajo.sql
@@ -13,35 +13,7 @@ with territorio_y_ventas as (
 
 ),
 
-ventas_por_territorio as (
-    select
-        distinct SalesOrderID,
-        TotalDue,
-        SubTotal,
-        OrderDate,
-        City,
-        AddressID,
-        Person_StateProvince.Name as Provincia,
-        Person_CountryRegion.Name as Pais
-
-    from Sales_SalesOrderHeader
-        join Person_Address on Person_Address.AddressID = Sales_SalesOrderHeader.BillToAddressID
-        join Person_StateProvince using (StateProvinceID)
-        join Person_CountryRegion using (CountryRegionCode)
-),
-
-ventas_y_monedas as (
-    select 
-        distinct SalesOrderID,
-        TotalDue,
-        SubTotal,
-        OrderDate,
-        FromCurrencyCode,
-        ToCurrencyCode
-    from Sales_SalesOrderHeader
-    join Sales_CurrencyRate using (CurrencyRateID)
-),
-
+-- ventas por producto
 Ventas_por_producto as (
     select 
         distinct ProductID,
@@ -55,11 +27,85 @@ Ventas_por_producto as (
         join Production_ProductCategory using (ProductCategoryID) 
         group by ProductID
 
+),
+
+-- Ventas y monedas
+ventas_y_monedas as (
+    select 
+        distinct SalesOrderID,
+        TotalDue,
+        SubTotal,
+        OrderDate,
+        FromCurrencyCode,
+        ToCurrencyCode
+    from Sales_SalesOrderHeader
+    join Sales_CurrencyRate using (CurrencyRateID)
+),
+
+-- Conssulta1 
+ventas_por_territorio as (
+    select
+        distinct SalesOrderID,
+        TotalDue,
+        OrderDate,
+        City,
+        AddressID,
+        Person_StateProvince.Name as Provincia,
+        Person_CountryRegion.Name as Pais
+
+    from Sales_SalesOrderHeader
+        join Person_Address on Person_Address.AddressID = Sales_SalesOrderHeader.BillToAddressID
+        join Person_StateProvince using (StateProvinceID)
+        join Person_CountryRegion using (CountryRegionCode)
+),
+
+
+ventas_por_producto_y_territorio as (
+    select
+    distinct ProductID,
+    OrderQty,
+    TotalDue,
+    OrderDate,
+    Production_Product.Name as producto,
+    Production_ProductCategory.Name as categoria,
+    Production_ProductSubcategory.Name as subcategoria,
+    City,
+    Provincia,
+    Pais
+    from ventas_por_territorio
+    join Sales_SalesOrderDetail using(SalesOrderID)
+    join Production_Product using (ProductID)
+    join Production_ProductSubcategory using (ProductSubcategoryID) 
+    join Production_ProductCategory using (ProductCategoryID) 
+    
+),
+
+-- Consulta2
+
+Utilidad_por_producto as (
+  
+    select 
+        distinct ProductID,
+        Production_Product.Name as producto,
+        sum(LineTotal) as Ingresos_prod,
+        sum(LineTotal-StandardCost*OrderQty) as utilidades,
+        Production_ProductCategory.Name as categoria,
+        Production_ProductSubcategory.Name as subcategoria
+        from Sales_SalesOrderDetail
+        join Production_Product using (ProductID)
+        join Production_ProductSubcategory using (ProductSubcategoryID) 
+        join Production_ProductCategory using (ProductCategoryID) 
+        group by ProductID
 )
+
+
+
+
+
 
 
 select *
 from
-Ventas_por_producto
+Utilidad_por_producto
 
-limit 30;
+limit 20;
